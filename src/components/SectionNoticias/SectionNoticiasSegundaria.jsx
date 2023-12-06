355a2335564d77acac1d4acd5ebf3ddb4ed0172c
Danilo Augusto <daniloaugusto9101@gmail.com>
2023-12-06 14:43:59 -0300

    Visulizacao das noticias arrumado

--- src/components/SectionNoticias/SectionNoticiasSegundaria.jsx
+++ src/components/SectionNoticias/SectionNoticiasSegundaria.jsx
@@ -1,22 +1,22 @@
-import React from "react"
 import useFetchPost from "../../hooks/useFetchPost"
 
 const SectionNoticiasSegundaria = () => {
-  const { mainPost, thumbnail } = useFetchPost(6, 5)
+  const { post, thumbnail } = useFetchPost(6, 5)
 
-  if (mainPost && thumbnail) {
+  if (post && thumbnail) {
     return (
       <>
-        {mainPost.map((obj, index) => (
-          <div className="bg-ternary-200 " key={index}>
+        {post.map((obj, index) => (
+          <div
+            className="bg-ternary-200 text-primary-900 hover:text-secondary-900"
+            key={index}
+          >
             <a
               href={obj.link}
               className="grid grid-cols-2 gap-2 items-center md:grid-cols-none"
             >
               <img src={thumbnail[index].guid.rendered} />
-              <h2 className=" text-primary-900 sm:text-xl">
-                {obj.title.rendered}
-              </h2>
+              <h2 className="  sm:text-xl">{obj.title.rendered}</h2>
             </a>
           </div>
         ))}
