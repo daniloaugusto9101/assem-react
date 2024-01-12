2f6fba9dd0a4737d8641dedc149adf4266680fa3
Danilo Augusto <daniloaugusto9101@gmail.com>
2024-01-12 11:17:27 -0300

    Integrção custom fild e agenda ajustada

--- src/components/Agenda/Agenda.jsx
+++ src/components/Agenda/Agenda.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import AgendaCard from "./AgendaCard"
 import Icons from "@components/Icons/Icons"
+import useFetchPost from "@/hooks/useFetchPost"
 const agendaPosts = [
   {
     dia: "20",
     mes: "ago",
-    titulo: "Noite do Queijo e Vinho",
+    titulo: "Festa das crianças e dos servidores 2024",
     local: "Sede Social",
   },
   {
@@ -35,19 +36,22 @@ const agendaPosts = [
 ]
 
 const Agenda = () => {
-  return (
-    <section className="container m-auto">
-      <div className="p-4 flex items-center gap-3">
-        <span className="text-4xl text-primary-900">
-          <Icons nomeDoIcone="FaCalendarDays" />
-        </span>
-        <h1 className="4 text-5xl font-bold text-primary-900">Agenda</h1>
-      </div>
-      <div className="p-4 mb-44 flex gap-2 overflow-y-auto ">
-        <AgendaCard agendaPosts={agendaPosts} />
-      </div>
-    </section>
-  )
+  const { post } = useFetchPost(9, 10)
+  if (post) {
+    return (
+      <section className="container m-auto">
+        <div className="p-4 flex items-center gap-3">
+          <span className="text-4xl text-primary-900">
+            <Icons nomeDoIcone="FaCalendarDays" />
+          </span>
+          <h1 className="4 text-5xl font-bold text-primary-900">Agenda</h1>
+        </div>
+        <div className="p-4 mb-44 flex gap-2 overflow-y-auto ">
+          <AgendaCard agendaPosts={post} />
+        </div>
+      </section>
+    )
+  }
 }
 
 export default Agenda
--- src/components/Agenda/AgendaCard.jsx
+++ src/components/Agenda/AgendaCard.jsx
@@ -8,17 +8,18 @@ const AgendaCard = ({ agendaPosts }) => {
         <div
           key={i}
           className="bg-ternary-50 p-2 shadow shadow-ternary-400 flex-none w-80 flex gap-2"
+          a
         >
           <div className="bg-primary-900 py-2 px-4 text-ternary-50 flex flex-col justify-center items-center">
-            <p className="text-5xl">{obj.dia}</p>
-            <p className="text-2xl">{obj.mes}</p>
+            <p className="text-5xl">{obj.acf.dia_do_evento}</p>
+            <p className="text-2xl">{obj.acf.mes_evento}</p>
           </div>
           <div>
-            <p className="font-bold text-xl">{obj.titulo}</p>
+            <p className="font-bold text-xl">{obj.acf.titulo_evento_agenda}</p>
 
             <p className="italic flex items-baseline text-ternary-700">
               <Icons nomeDoIcone="FaLocationDot" />
-              <span>{obj.local}</span>
+              <span>{obj.acf.local_do_evento}</span>
             </p>
           </div>
         </div>
